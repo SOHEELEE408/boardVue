47d7e23bd8de23e6df242ac7d081e599f481f3a0
sohee <dlwlsrl33@gmail.com>
2022-01-31 15:35:54 +0900

    initial commit

---
+++ src/test/java/com/boardvue/BoardVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
