33adbd7633836de4cd40e76fc8fb646ad2a74a3e
sohee <dlwlsrl33@gmail.com>
2022-02-01 16:43:22 +0900

    board CRUD api 생성

---
+++ src/main/java/com/boardvue/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.boardvue.web;
+
+import com.boardvue.service.PostsService;
+import com.boardvue.web.dto.PostsResponseDto;
+import com.boardvue.web.dto.PostsSaveRequestDto;
+import com.boardvue.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
---
+++ src/main/java/com/boardvue/web/dto/PostsResponseDto.java
@@ -0,0 +1,17 @@
+package com.boardvue.web.dto;
+
+import com.boardvue.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title, content, author;
+
+    public PostsResponseDto(Posts entity){
+        this.id=entity.getId();
+        this.title=entity.getTitle();
+        this.content=entity.getContent();
+        this.author=entity.getAuthor();
+    }
+}
